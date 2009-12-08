bd2e1c1150ead4be7fb610e21c23edb6a92e34bf
Jarod Luebbert <jarod.luebbert@gmail.com>
2009-12-08 12:10:08 -0600

    fixed the current tab bug. still using session to store current course. need to fix that.

--- app/controllers/assignments_controller.rb
+++ app/controllers/assignments_controller.rb
@@ -2,6 +2,7 @@ class AssignmentsController < ApplicationController
 
   def show
     @assignment = Assignment.find(params[:course_id])
+    session[:current_id] = @assignment.course_id
   end
 
 end
